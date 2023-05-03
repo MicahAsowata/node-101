6e6a761e4da414db50ff20463eeaf6665ba518b3
Micah Asowata <andrewgary963@gmail.com>
2023-05-03 13:15:53 +0100

    Deleted unnecessay folders and trying to create a folder

--- basics.js
+++ basics.js
@@ -1,24 +1,9 @@
 const fs = require("fs");
-const file = (dir, filename) => {
-  fs.mkdir(`./${dir}`, (err) => {
-    fs.writeFile(
-      `./${dir}/${filename}.txt`,
-      `Hurray, ${__filename} has being created`,
-      () => {
-        fs.readFile(`./${dir}/${filename}.txt`, (err, data) => {
-          if (err) {
-            console.log(err.message);
-          }
 
-          console.log(data.toString());
-        });
-      }
-    );
-    if (err) {
-      console.log(err.message);
-    }
-  });
-};
+fs.mkdir("./assets", (err) => {
+  if (err.code === -4075) {
+    console.log("Directory exists");
+  }
 
-console.log("writing file....");
-file("docy", "blogFour");
+  console.log("Directory successfully created");
+});
