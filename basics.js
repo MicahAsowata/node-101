67bbe416091d9f3e7f409f0b08c5e82a70df73e2
Micah Asowata <andrewgary963@gmail.com>
2023-05-03 23:50:02 +0100

    Piping streams

--- basics.js
+++ basics.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
 const readStream = fs.createReadStream("./docs/blog.txt", { encoding: "utf8" });
 const writeStream = fs.createWriteStream("./docs/blogOne.txt");
-readStream.on("data", (chunk) => {
-  console.log("-- New chunk --");
-  console.log(chunk);
-  writeStream("\nNEW CHUNK\n");
-  writeStream.write(chunk);
-});
+
+readStream.pipe(writeStream);
