97916d072f7c23f1c1f45f91b8cd64f40df67119
Micah Asowata <andrewgary963@gmail.com>
2023-05-03 14:09:15 +0100

    Reading a data stream

--- basics.js
+++ basics.js
@@ -1,1 +1,7 @@
 const fs = require("fs");
+const readStream = fs.createReadStream("./docs/blog.txt");
+
+readStream.on("data", (chunk) => {
+  console.log("-- New chunk --");
+  console.log(chunk.toString());
+});
